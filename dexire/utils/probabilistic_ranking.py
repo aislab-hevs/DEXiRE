c1d77dbf2d3aba993ad65a805f9e976c6b649843
Victor Contreras <victorc365@gmail.com>
2024-05-06 16:03:22 +0200

    Added test probabilistic rank.

--- dexire/utils/probabilistic_ranking.py
+++ dexire/utils/probabilistic_ranking.py
@@ -5,25 +5,51 @@ from collections import Counter
 def probabilistic_ranking(dict_path_layer: Dict, key: str ='support') -> List[Tuple]:
     """Sort an activation path of values based on a given key.
 
-    Args:
-        dict_path_layer (Dict): Dictionary for Layer i. 
-        key (str, optional): Key to sort the values. Defaults to 'support'.
-
-    Returns:
-        List[Tuple]: List of sorted values.
+    :param dict_path_layer: Dictionary containing the neurons to process.
+    :type dict_path_layer: Dict
+    :param key: Key to sort the values, defaults to 'support'
+    :type key: str, optional
+    :return: List of sorted values.
+    :rtype: List[Tuple]
     """
     sort_out = sorted(dict_path_layer.items(), key=lambda item: item[1][key], reverse=True)
     return sort_out
 
 def entropy(labels:np.array) -> float:
-    """Calculate entropy of a list of labels."""
+    """Calculate entropy of a list of labels.
+
+    :param labels: Label array.
+    :type labels: np.array
+    :return: Entropy of label distribution.
+    :rtype: float
+    """
     n = len(labels)
     counts = np.bincount(labels)
     probabilities = counts[np.nonzero(counts)] / n
     return -np.sum(probabilities * np.log2(probabilities))
 
-def information_gain(parent_labels: np.array, children_labels:np.array):
-    """Calculate information gain."""
+def information_gain(parent_labels: np.array, children_labels:np.array) -> float:
+    """Calculate information gain.
+
+    :param parent_labels: labels before splitting.
+    :type parent_labels: np.array
+    :param children_labels: List of label splits.
+    :type children_labels: np.array
+    :return: Information gain for the given split
+    :rtype: float
+    """
     parent_entropy = entropy(parent_labels)
     children_entropy = sum((len(child) / len(parent_labels)) * entropy(child) for child in children_labels)
     return parent_entropy - children_entropy
+
+def calculate_information_gain_per_feature(X: np.array, y: np.array):
+    #TODO: Finish to make it efficient
+    dict_answer = {}
+    # original entropy 
+    original_entropy = entropy(y)
+    for feature_idx in range(X.shape[1]):
+        unique_values = np.unique(X[:, feature_idx])
+        for val in unique_values:
+            left_indices = X[:, feature_idx] == val
+            right_indices = X[:, feature_idx] != val
+    pass
---
+++ test/test_probabilistic_ranking.py
@@ -0,0 +1,66 @@
+# Test one rule with expressions and clauses. 
+import os
+import pytest
+import sys
+import numpy as np
+from sklearn.datasets import load_iris, make_classification, make_regression
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from dexire.utils.probabilistic_ranking import probabilistic_ranking, entropy, information_gain
+from dexire.utils.activation_discretizer import discretize_activation_layer
+
+@pytest.fixture
+def generate_binary_data():
+    X, y = make_classification(random_state=49)
+    return X, y
+
+@pytest.fixture
+def generate_multi_class_data():
+    X, y = load_iris(return_X_y=True)
+    return X, y
+
+def test_entropy_on_binary_data(generate_binary_data):
+    X, y = generate_binary_data
+    predicted = entropy(y)
+    assert predicted >= 0 and predicted <= 1.0
+    
+
+def test_entropy_on_multi_class_data(generate_multi_class_data):
+    X, y = generate_multi_class_data
+    predicted = entropy(y)
+    assert predicted >= 0
+
+def test_information_gain_on_binary_data(generate_binary_data):
+    X, y = generate_binary_data
+    # binarize 
+    X_binary = discretize_activation_layer(X, n_bins=5)
+    val, count  = np.unique(X_binary[:, 0], return_counts=True) 
+    split_value = val[np.argmax(count)]
+    condition = X_binary[:, 0] == split_value
+    y_left = y[condition]
+    y_right = y[~condition]
+    predicted = information_gain(y, [y_left, y_right])
+    assert predicted >= 0 and predicted <= 1.0
+
+def test_information_gain_on_multi_class_data(generate_multi_class_data):
+    X, y = generate_multi_class_data
+    # binarize 
+    X_binary = discretize_activation_layer(X, n_bins=5)
+    val, count  = np.unique(X_binary[:, 0], return_counts=True) 
+    split_value = val[np.argmax(count)]
+    condition = X_binary[:, 1] == split_value
+    y_left = y[condition]
+    y_right = y[~condition]
+    predicted = information_gain(y, [y_left, y_right])
+    assert predicted >= 0 and predicted <= 1.0
+    
+def test_probabilistic_ranking(generate_binary_data):
+    X, y = generate_binary_data
+    # Binarize 
+    X_bin = discretize_activation_layer(X, n_bins=2)
+    # Ranking
+    dict_values = {}
+    for i in range(X_bin.shape[1]):
+        dict_values[i] = probabilistic_ranking(X_bin[:, i])
+    
+    pass
